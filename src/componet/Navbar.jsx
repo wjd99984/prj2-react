b843714f25b0c29e80f951efec5e51d70aff17b3
jung tae wook <xodnr901901@naver.com>
2024-05-21 11:25:03 +0900

    Navbar.jsx 작성

---
+++ src/componet/Navbar.jsx
@@ -0,0 +1,30 @@
+import { useNavigate } from "react-router-dom";
+import { Box, Flex } from "@chakra-ui/react";
+import React from "react";
+
+export function Navbar() {
+  const navigate = useNavigate();
+
+  return (
+    <Flex gap={3}>
+      <Box
+        onClick={() => navigate("/")}
+        cursor={"pointer"}
+        _hover={{
+          bgColor: "gray.200",
+        }}
+      >
+        HOME
+      </Box>
+      <Box
+        onClick={() => navigate("/write")}
+        cursor={"pointer"}
+        _hover={{
+          bgColor: "gray.200",
+        }}
+      >
+        글쓰기
+      </Box>
+    </Flex>
+  );
+}
--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,10 +1,12 @@
 import { Box } from "@chakra-ui/react";
 import { Outlet } from "react-router-dom";
 import React from "react";
+import { Navbar } from "../componet/Navbar.jsx";
 
 export function Home() {
   return (
     <Box>
+      <Navbar />
       <Box>navbar</Box>
       <Box>
         <Outlet />
