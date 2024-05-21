de88dbf8b40422108023127e1b9af8da84aa3993
jung tae wook <xodnr901901@naver.com>
2024-05-21 12:38:02 +0900

    제목 본문 작성자가 비어있으면 저장버튼 비활성화

--- src/page/board/BoardWrite.jsx
+++ src/page/board/BoardWrite.jsx
@@ -33,8 +33,29 @@ export function BoardWrite() {
         });
         navigate("/");
       })
-      .catch()
+      .catch((e) => {
+        const code = e.response.status;
+
+        if (code === 4000) {
+          toast({
+            status: "error",
+            description: "등록되지 않았습니다. 입력한 내용을 확인하세요.",
+            position: "top",
+          });
+        }
+      })
       .finally();
+  }
+
+  let disableSaveButton = false;
+  if (title.trim().length === 0) {
+    disableSaveButton = true;
+  }
+  if (content.trim().length === 0) {
+    disableSaveButton = true;
+  }
+  if (writer.trim().length === 0) {
+    disableSaveButton = true;
   }
 
   return (
@@ -60,7 +81,11 @@ export function BoardWrite() {
           </FormControl>
         </Box>
         <Box>
-          <Button colorScheme={"blue"} onClick={handleSaveClick}>
+          <Button
+            isDisabled={disableSaveButton}
+            colorScheme={"blue"}
+            onClick={handleSaveClick}
+          >
             저장
           </Button>
         </Box>
