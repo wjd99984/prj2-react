1083e4b0c8889e65bbd563f0b05b9ced5c792cf3
jung tae wook <xodnr901901@naver.com>
2024-05-22 10:50:09 +0900

    닉네임 사용 가능한 지 확인
    404 -> 없으니까 사용 가능
    200 -> 있으니까 사용 불가능

--- src/page/member/MemberSignup.jsx
+++ src/page/member/MemberSignup.jsx
@@ -76,6 +76,28 @@ export function MemberSignup() {
       .finally();
   }
 
+  function handleCheckNickname() {
+    axios
+      .get(`/api/member/check?nickName=${nickName}`)
+      .then((res) => {
+        toast({
+          status: "warning",
+          description: "사용할 수 없는 닉네임입니다.",
+          position: "top",
+        });
+      })
+      .catch((err) => {
+        if (err.response.status === 404) {
+          toast({
+            status: "info",
+            description: "사용할 수 있는 닉네임입니다.",
+            position: "top",
+          });
+        }
+      })
+      .finally();
+  }
+
   return (
     <Box>
       <Box>회원 가입</Box>
@@ -102,7 +124,14 @@ export function MemberSignup() {
         <Box>
           <FormControl>
             <FormLabel>별명</FormLabel>
-            <Input onChange={(e) => setNickName(e.target.value)} />
+            <InputGroup>
+              <Input onChange={(e) => setNickName(e.target.value)} />
+              <InputRightElement w={"75px"} mr={1}>
+                <Button onClick={handleCheckNickname} size={"sm"}>
+                  중복확인
+                </Button>
+              </InputRightElement>
+            </InputGroup>
           </FormControl>
         </Box>
         <Box>
