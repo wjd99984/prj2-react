bd3d057575f0b7501af872692484b3b31f72e4ab
jung tae wook <xodnr901901@naver.com>
2024-05-23 14:37:55 +0900

    로그인 시도 후 응답 코드에 따른 로직 작성
    200 : localStorage 에 토큰 저장 후 메인으로 이동
    나머지 : localStroage 에서 토큰 삭제

--- src/page/member/MemberLogin.jsx
+++ src/page/member/MemberLogin.jsx
@@ -19,9 +19,23 @@ export function MemberLogin() {
   function handleLogin() {
     axios
       .post("/api/member/token", { email, password })
-      .then((res) => {})
-      .catch(() => {})
-      .finally(() => {});
+      .then((res) => {
+        localStorage.setItem("token", res.data.token);
+        toast({
+          status: "success",
+          description: "로그인 되었습니다.",
+          position: "top",
+        });
+        navigate("/");
+      })
+      .catch(() => {
+        localStorage.removeItem("token");
+        toast({
+          status: "warning",
+          description: "이메일과 패스워드를 확인해주세요.",
+          position: "top",
+        });
+      });
   }
 
   return (
