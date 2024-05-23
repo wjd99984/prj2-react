570e2a0181bf6f9b3f219f5bf815b5e5a39dcdf0
jung tae wook <xodnr901901@naver.com>
2024-05-23 12:20:06 +0900

    수정 후 성공.실패 로직 작성 성공시 토스트 출력 , 회원정보보기로 이동 실패시 토스트 출력

--- src/page/member/MemberEdit.jsx
+++ src/page/member/MemberEdit.jsx
@@ -53,9 +53,25 @@ export function MemberEdit() {
   function handleClickSave() {
     axios
       .put("/api/member/modify", { ...member, oldPassword })
-      .then((res) => {})
-      .catch(() => {})
-      .finally(() => {});
+      .then((res) => {
+        toast({
+          status: "success",
+          description: "회원정보 수정",
+          position: "top",
+        });
+        navigate("/member/modify");
+      })
+      .catch(() => {
+        toast({
+          status: "error",
+          description: "회원정보 수정 실패",
+          position: "top",
+        });
+      })
+      .finally(() => {
+        onClose();
+        setOldPassword("");
+      });
   }
 
   if (member === null) {
