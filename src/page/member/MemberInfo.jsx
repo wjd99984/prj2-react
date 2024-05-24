5a32db8bd78a742bcc27df561ad1bf97a3fed836
jung tae wook <xodnr901901@naver.com>
2024-05-24 10:04:44 +0900

    탈퇴후 로그아웃

--- src/page/member/MemberInfo.jsx
+++ src/page/member/MemberInfo.jsx
@@ -14,14 +14,16 @@ import {
   useDisclosure,
   useToast,
 } from "@chakra-ui/react";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import axios from "axios";
 import { useNavigate, useParams } from "react-router-dom";
+import { LoginContext } from "../../component/LoginProvider.jsx";
 
 export function MemberInfo() {
   const [member, setMember] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   const [password, setPassword] = useState("");
+  const account = useContext(LoginContext);
   const { id } = useParams();
   const toast = useToast();
   const navigate = useNavigate();
@@ -59,6 +61,7 @@ export function MemberInfo() {
           description: "회원 탈퇴하였습니다.",
           position: "top",
         });
+        account.logout();
         navigate("/");
       })
       .catch(() => {
