43e2a189bb74ab29a472cfa794b7ae535d1890aa
jung tae wook <xodnr901901@naver.com>
2024-05-27 12:16:06 +0900

    처음버튼

--- src/page/board/BoardList.jsx
+++ src/page/board/BoardList.jsx
@@ -57,9 +57,14 @@ export function BoardList() {
       </Box>
       <Box>
         {pageInfo.prevPageNumber && (
-          <Button onClick={() => navigate(`/?page=${pageInfo.prevPageNumber}`)}>
-            이전
-          </Button>
+          <>
+            <Button onClick={() => navigate(`/?page=1`)}>처음</Button>
+            <Button
+              onClick={() => navigate(`/?page=${pageInfo.prevPageNumber}`)}
+            >
+              이전
+            </Button>
+          </>
         )}
         {pageNumbers.map((pageNumber) => (
           <Button
