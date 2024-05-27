9f151a443aa2e2d31b454fc77689eb5c9c2a4979
jung tae wook <xodnr901901@naver.com>
2024-05-27 09:46:20 +0900

    게시물 목록에서 페이지 번호 넘기기
    페이지 번호 클릭시 해당 페이지 게시물 목록 출력

--- src/page/board/BoardList.jsx
+++ src/page/board/BoardList.jsx
@@ -1,17 +1,20 @@
-import { Box, Table, Tbody, Td, Th, Thead, Tr } from "@chakra-ui/react";
+import { Box, Button, Table, Tbody, Td, Th, Thead, Tr } from "@chakra-ui/react";
 import React, { useEffect, useState } from "react";
 import { faUserPen } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { customAxios as axios } from "../../axiosInstance.jsx";
-import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { useNavigate, useSearchParams } from "react-router-dom";
 
 export function BoardList() {
   const [boardList, setBoardList] = useState([]);
   const navigate = useNavigate();
+  const [searchParams] = useSearchParams();
 
   useEffect(() => {
-    axios.get("/api/board/list").then((res) => setBoardList(res.data));
-  }, []);
+    axios
+      .get(`/api/board/list?${searchParams}`)
+      .then((res) => setBoardList(res.data));
+  }, [searchParams]);
 
   return (
     <Box>
@@ -44,6 +47,16 @@ export function BoardList() {
             ))}
           </Tbody>
         </Table>
+      </Box>
+      <Box>
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((pageNumber) => (
+          <Button
+            onClick={() => navigate(`/?page=${pageNumber}`)}
+            key={pageNumber}
+          >
+            {pageNumber}
+          </Button>
+        ))}
       </Box>
     </Box>
   );
