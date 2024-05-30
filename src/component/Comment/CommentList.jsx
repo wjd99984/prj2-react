5da73924dd38953b73c7d05c42a2e0256cca1012
jung tae wook <xodnr901901@naver.com>
2024-05-30 11:23:11 +0900

    댓글 목록

--- src/component/Comment/CommentList.jsx
+++ src/component/Comment/CommentList.jsx
@@ -1,3 +1,33 @@
-export function CommentList() {
-  return null;
+import { useEffect } from "react";
+import axios from "axios";
+import { Box, Flex, Spacer } from "@chakra-ui/react";
+
+export function CommentList({ boardId }) {
+  const [commentList, setCommentList] = useState();
+  useEffect(() => {
+    axios
+      .get(`/api/comment/list/${boardId}`)
+      .then((res) => {
+        setCommentList(res.data);
+      })
+      .catch((err) => console.log(err))
+      .finally(() => {});
+  }, []);
+  if (commentList.length === 0) {
+    return <Box>댓글이없음 첫댓글 작성</Box>;
+  }
+  return (
+    <Box>
+      {commentList.map((comment) => (
+        <Box key={comment.id} border="{1px solid bule}" my={3}>
+          <Flex>
+            <Box>{comment.memberId}</Box>
+            <Spacer />
+            <Box>{comment.inserted} </Box>
+          </Flex>
+          <Box>{comment.comment}</Box>
+        </Box>
+      ))}
+    </Box>
+  );
 }
