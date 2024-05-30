4529e675b21a1820de7c807c676b107828f3f0ba
jung tae wook <xodnr901901@naver.com>
2024-05-30 12:31:36 +0900

    댓글 목록

--- src/component/Comment/CommentList.jsx
+++ src/component/Comment/CommentList.jsx
@@ -1,9 +1,9 @@
-import { useEffect } from "react";
-import axios from "axios";
+import { useEffect, useState } from "react";
 import { Box, Flex, Spacer } from "@chakra-ui/react";
+import axios from "axios";
 
 export function CommentList({ boardId }) {
-  const [commentList, setCommentList] = useState();
+  const [commentList, setCommentList] = useState([]);
   useEffect(() => {
     axios
       .get(`/api/comment/list/${boardId}`)
@@ -14,16 +14,16 @@ export function CommentList({ boardId }) {
       .finally(() => {});
   }, []);
   if (commentList.length === 0) {
-    return <Box>댓글이없음 첫댓글 작성</Box>;
+    return <Box>댓글이 없습니다. 첫 댓글을 작성해보세요.</Box>;
   }
   return (
     <Box>
       {commentList.map((comment) => (
-        <Box key={comment.id} border="{1px solid bule}" my={3}>
+        <Box key={comment.id} border={"1px solid black"} my={3}>
           <Flex>
             <Box>{comment.memberId}</Box>
             <Spacer />
-            <Box>{comment.inserted} </Box>
+            <Box>{comment.inserted}</Box>
           </Flex>
           <Box>{comment.comment}</Box>
         </Box>
