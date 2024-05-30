ebf72a881c9eb46d715456f635de3e7d97b1bcc1
jung tae wook <xodnr901901@naver.com>
2024-05-30 12:14:49 +0900

    댓글 목록

--- src/component/Comment/CommentComponent.jsx
+++ src/component/Comment/CommentComponent.jsx
@@ -6,7 +6,7 @@ export function CommentComponent({ boardId }) {
   return (
     <Box>
       <CommentWrite boardId={boardId} />
-      <CommentList />
+      <CommentList boardId={boardId} />
     </Box>
   );
 }
